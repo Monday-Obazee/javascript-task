489066ab10c7e50c8dc0a7e1885e21e601498fbd
Monday-Obazee <Mondayobazee@gmail.com>
2020-03-23 21:04:31 -0800

    index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+var myNameIs = "Monday Osas Obazee"
+var myNameIs = document.getElementById("myname").innerHTML = myNameIs;
+
+
+
+
+var coursesRegistered = ["html/css/javascript", "coding"];
+var coursesRegistered = document.getElementById("coursesregistered").innerHTML = coursesRegistered;
+
+
+var text = "";
+var y;
+for (y = 2; y < 200; y++,y++) {
+  text += " " + y +  "<br>";
+}
+
+var text = document.getElementById("even").innerHTML = text;
